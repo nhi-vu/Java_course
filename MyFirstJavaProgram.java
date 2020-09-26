b9cd19e213d065262d9b2d830781e76b9875fb73
Nhi Vu <67386036+nhi-vu@users.noreply.github.com>
2020-09-25 22:47:52 -0700

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,14 @@
+/*
+ * Nhi Vu
+ * CS A170
+ * Sep 02 2020
+ * 
+ * Exercise #01
+ */
+public class MyFirstJavaProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is my first Java Program!"); 
+    }
+}
