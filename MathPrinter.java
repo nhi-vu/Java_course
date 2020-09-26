d83a05649d2b6c298de675fc658fd61c6d9b2f71
Nhi Vu <67386036+nhi-vu@users.noreply.github.com>
2020-09-25 22:47:32 -0700

    Add files via upload

---
+++ MathPrinter.java
@@ -0,0 +1,15 @@
+/*
+ * Nhi Vu
+ * CS A170
+ * Sep 02 2020
+ * 
+ * Exercise #01
+ */
+public class MathPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1+3+5+7+9); 
+        System.out.println(1*3*5*7*9);
+    }
+}
